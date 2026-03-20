fad39617c8a311b538f871b1086ab7be64ba31e2
Andrzej <andrzej10@schule.bremen.de>
2026-03-20 10:03:21 +0100

    fix: import correct prisma path

--- packages/backend/src/lib/prisma.ts
+++ packages/backend/src/lib/prisma.ts
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
-// import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
